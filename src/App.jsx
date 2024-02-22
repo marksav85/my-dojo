fdca2395fd4f5b33d69ba70ac34587c36b3732e7
Mark Saville <mark.saville@outlook.com>
2024-02-22 19:12:01 +0100

    Refactor App component to use react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// components
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Create from "./pages/create/Create";
+import Project from "./pages/project/Project";
+
 // styles
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <header className="App-header"></header>
+    <div className="App">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Dashboard />}></Route>
+          <Route path="/create" element={<Create />}></Route>
+          <Route path="/projects/:id" element={<Project />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
+        </Routes>
+      </div>
     </div>
   );
 }
