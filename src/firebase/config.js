1eedb673a41e14fa7ba956fadc864882f28d6bc8
Mark Saville <mark.saville@outlook.com>
2024-02-22 17:45:42 +0100

    Add Firebase configuration file

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyChjaNrr6Tp3uHRQnMoDLx7GZPjOA9Vdyw",
+  authDomain: "my-dojo-fc275.firebaseapp.com",
+  projectId: "my-dojo-fc275",
+  storageBucket: "my-dojo-fc275.appspot.com",
+  messagingSenderId: "537956070823",
+  appId: "1:537956070823:web:32c149e0c0dc0a0e86f282",
+};
+
+// init firebase
+const app = initializeApp(firebaseConfig);
+
+// init services
+const myFSProject = getFirestore(app);
+const myFSAuth = getAuth(app);
+
+export { myFSProject, myFSAuth };
