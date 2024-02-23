452de6b557ac9653c7660a8af162a52f4f32f3dc
Mark Saville <mark.saville@outlook.com>
2024-02-23 10:02:13 +0100

    Add signup form styles and functionality

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
+
 // styles
 import "./Signup.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState(null);
+
+  return (
+    <form className="auth-form">
+      <h2>Signup</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>profile thumbnail:</span>
+        <input type="file" required />
+      </label>
+      <button className="btn">Signup</button>
+    </form>
+  );
 }
