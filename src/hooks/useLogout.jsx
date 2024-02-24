195a1d7df0827a38c579d2ea88312e4c07b6f417
Mark Saville <mark.saville@outlook.com>
2024-02-24 18:22:11 +0100

    Add online status update on logout

--- src/hooks/useLogout.jsx
+++ src/hooks/useLogout.jsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import { useAuthContext } from "./useAuthContext";
-import { myFSAuth } from "../firebase/config";
+import { myFSAuth, myFSProject } from "../firebase/config";
+import { doc, updateDoc } from "firebase/firestore";
 
 // custom hook to handle logout
 export const useLogout = () => {
   const [isCancelled, setIsCancelled] = useState(false);
   const [error, setError] = useState(null);
   const [isPending, setIsPending] = useState(false);
-  const { dispatch } = useAuthContext();
+  const { dispatch, user } = useAuthContext();
 
   // handle logout
   const handleLogout = async () => {
@@ -16,6 +17,16 @@ export const useLogout = () => {
 
     // sign user out
     try {
+      // update online status
+      const { uid } = user;
+      // Assuming you have the UID in the variable 'uid'
+      const userDocRef = doc(myFSProject, "users", uid);
+
+      // Update the document
+      await updateDoc(userDocRef, {
+        online: false,
+      });
+
       await myFSAuth.signOut();
 
       // dispatch logout action
