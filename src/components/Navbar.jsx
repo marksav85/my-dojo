3260c06d4610f5fa44ee3b602d1dc72523d64d88
Mark Saville <mark.saville@outlook.com>
2024-02-25 12:06:34 +0100

    Refactor Navbar component to conditionally display login, signup, and logout links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,59 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // styles and images
 import "./Navbar.css";
 import Temple from "../assets/temple.svg";
 
+// Functional component for the Navbar
 export default function Navbar() {
+  // Destructuring values from the useLogout hook
   const { handleLogout, isPending } = useLogout();
+
+  // Destructuring user from the useAuthContext hook
+  const { user } = useAuthContext();
+
+  // Render the Navbar component
   return (
     <div className="navbar">
       <ul>
+        {/* Logo section */}
         <li className="logo">
           <img src={Temple} alt="dojo logo" />
           <span>The Dojo</span>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button className="btn" onClick={handleLogout}>
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button className="btn" disabled onClick={handleLogout}>
-              Logout
-            </button>
-          )}
-        </li>
+
+        {/* Display login and signup links if there is no user */}
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {/* Display logout button if there is a user */}
+        {user && (
+          <>
+            <li>
+              {/* Render logout button with appropriate styling and disabled state based on isPending */}
+              {!isPending && (
+                <button className="btn" onClick={handleLogout}>
+                  Logout
+                </button>
+              )}
+              {isPending && (
+                <button className="btn" disabled onClick={handleLogout}>
+                  Logout
+                </button>
+              )}
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
