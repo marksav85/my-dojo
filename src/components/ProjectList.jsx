b497f6862e8175f8662303c0be6feb781093726b
Mark Saville <mark.saville@outlook.com>
2024-02-27 13:10:45 +0100

    Add Link component and display project details in ProjectList

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,3 +1,8 @@
+import { Link } from "react-router-dom";
+
+// components
+import Avatar from "./Avatar";
+
 // styles
 import "./ProjectList.css";
 
@@ -6,7 +11,20 @@ export default function ProjectList({ projects }) {
     <div>
       {projects.length === 0 && <p> No projects yet!</p>}
       {projects.map((project) => (
-        <div key={project.id}></div>
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p>Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className="assigned-to">
+            <p>Assigned to:</p>
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} alt={user.displayName} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
