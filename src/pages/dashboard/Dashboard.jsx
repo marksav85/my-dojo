613b8cd6367e601cebd18a87be18996d700535b1
Mark Saville <mark.saville@outlook.com>
2024-02-27 12:40:15 +0100

    Add useCollection hook to Dashboard component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,16 @@
+import { useCollection } from "../../hooks/useCollection";
+
 // styles
 import "./Dashboard.css";
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && documents.map((doc) => <p key={doc.id}>{doc.name}</p>)}
+    </div>
+  );
 }
