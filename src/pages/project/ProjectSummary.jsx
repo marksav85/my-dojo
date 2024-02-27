64cb6dfd82b23d14ceb8d101302a03dd6b38030c
Mark Saville <mark.saville@outlook.com>
2024-02-27 15:10:24 +0100

    Add ProjectSummary component

---
+++ src/pages/project/ProjectSummary.jsx
@@ -0,0 +1,26 @@
+import Avatar from "../../components/Avatar";
+
+// styles
+import "./Project.css";
+
+export default function ProjectSummary({ project }) {
+  return (
+    <div>
+      <div className="project-summary">
+        <h2 className="page-title">{project.name}</h2>
+        <p className="due-date">
+          Project due by {project.dueDate.toDate().toDateString()}
+        </p>
+        <p className="details">{project.details}</p>
+        <h4>Project is assigned to:</h4>
+        <div className="assigned-users">
+          {project.assignedUsersList.map((user) => (
+            <div key={user.id}>
+              <Avatar src={user.photoURL} alt={user.displayName} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
