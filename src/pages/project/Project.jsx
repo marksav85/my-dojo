b9be13ce4de15f8399d539e1c63334c30376fe27
Mark Saville <mark.saville@outlook.com>
2024-02-27 14:42:46 +0100

    Add dynamic project details to Project component

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -1,6 +1,23 @@
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+
 // styles
 import "./Project.css";
 
 export default function Project() {
-  return <div>Project</div>;
+  const { id } = useParams();
+  const { error, document } = useDocument("projects", id);
+
+  if (error) {
+    return <div>{error.message}</div>;
+  }
+  if (!document) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="project-details">
+      <h1>{document.name}</h1>
+    </div>
+  );
 }
