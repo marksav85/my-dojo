223bc198c8b3d6ae15579798db9cb4778c70a6ea
Mark Saville <mark.saville@outlook.com>
2024-02-28 10:40:29 +0100

    Add useFirestore hook and update comment submission logic

--- src/pages/project/ProjectComments.jsx
+++ src/pages/project/ProjectComments.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import { Timestamp } from "firebase/firestore";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useFirestore } from "../../hooks/useFirestore";
 
-export default function ProjectComments() {
+export default function ProjectComments({ project }) {
+  const { response, updateDocument } = useFirestore("projects");
   const [newComment, setNewComment] = useState("");
   const { user } = useAuthContext();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("project", project);
 
     const commentToAdd = {
       displayName: user.displayName,
@@ -16,7 +19,13 @@ export default function ProjectComments() {
       createdAt: Timestamp.now(),
       id: Math.random(),
     };
-    console.log(commentToAdd);
+
+    await updateDocument(project.id, {
+      comments: [...project.comments, commentToAdd],
+    });
+    if (!response.error) {
+      setNewComment("");
+    }
   };
 
   return (
