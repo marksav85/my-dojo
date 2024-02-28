c7e107e059b8a078117c17a67cce9b7e43e02ef8
Mark Saville <mark.saville@outlook.com>
2024-02-28 16:07:46 +0100

    Update comments

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -6,19 +6,31 @@ import Avatar from "./Avatar";
 // styles
 import "./ProjectList.css";
 
+// Component for displaying a list of projects
 export default function ProjectList({ projects }) {
+  // Render the project list component
   return (
     <div className="project-list">
+      {/* Display a message if there are no projects */}
       {projects.length === 0 && <p> No projects yet!</p>}
+
+      {/* Map through projects and render details for each project */}
       {projects.map((project) => (
+        // Link to the detailed view of the project
         <Link to={`/projects/${project.id}`} key={project.id}>
+          {/* Project title */}
           <h4>{project.name}</h4>
+
+          {/* Project due date */}
           <p>Due by {project.dueDate.toDate().toDateString()}</p>
+
+          {/* List of assigned users with avatars */}
           <div className="assigned-to">
             <p>Assigned to:</p>
             <ul>
               {project.assignedUsersList.map((user) => (
                 <li key={user.photoURL}>
+                  {/* Display user avatar */}
                   <Avatar src={user.photoURL} alt={user.displayName} />
                 </li>
               ))}
--- src/pages/dashboard/ProjectFilter.jsx
+++ src/pages/dashboard/ProjectFilter.jsx
@@ -1,6 +1,7 @@
 // styles
 import "./Dashboard.css";
 
+// List of available project filter options
 const filterList = [
   "all",
   "mine",
@@ -10,20 +11,27 @@ const filterList = [
   "sales",
 ];
 
+// Component for filtering projects based on categories
 export default function ProjectFilter({ currentFilter, changeFilter }) {
+  // Function to handle button click and update the filter
   const handleClick = (newFilter) => {
     changeFilter(newFilter);
   };
 
+  // Render the project filter component
   return (
     <div className="project-filter">
+      {/* Navigation bar for project filtering */}
       <nav>
+        {/* Display a label for the filter options */}
         <p>Filter by:</p>
+
+        {/* Map through filterList and render a button for each filter option */}
         {filterList.map((filter) => (
           <button
             key={filter}
-            onClick={() => handleClick(filter)}
-            className={currentFilter === filter ? "active" : ""}
+            onClick={() => handleClick(filter)} // Handle click event to update the filter
+            className={currentFilter === filter ? "active" : ""} // Apply 'active' class if the filter is currently selected
           >
             {filter}
           </button>
