17f349a0c55bddce225d2126d805623862283255
Mark Saville <mark.saville@outlook.com>
2024-02-28 15:56:40 +0100

    Update comments for clarity

--- src/App.jsx
+++ src/App.jsx
@@ -14,41 +14,65 @@ import OnlineUsers from "./components/OnlineUsers";
 // styles
 import "./App.css";
 
+// Main App component
 function App() {
+  // Retrieve user and authIsReady from the authentication context
   const { user, authIsReady } = useAuthContext();
 
   return (
     <div className="App">
+      {/* Render the app contents only when authentication is ready */}
       {authIsReady && (
         <>
+          {/* Render Sidebar component if the user is authenticated */}
           {user && <Sidebar />}
+
+          {/* Main container for the application */}
           <div className="container">
+            {/* Render the Navbar component */}
             <Navbar></Navbar>
+
+            {/* Define routes for the application */}
             <Routes>
               <Route
                 path="/"
                 element={
+                  // Redirect to login if the user is not authenticated, otherwise render Dashboard
                   !user ? <Navigate to="/login" replace /> : <Dashboard />
                 }
               ></Route>
               <Route
                 path="/create"
-                element={!user ? <Navigate to="/login" replace /> : <Create />}
+                element={
+                  // Redirect to login if the user is not authenticated, otherwise render Create
+                  !user ? <Navigate to="/login" replace /> : <Create />
+                }
               ></Route>
               <Route
                 path="/projects/:id"
-                element={!user ? <Navigate to="/login" replace /> : <Project />}
+                element={
+                  // Redirect to login if the user is not authenticated, otherwise render Project
+                  !user ? <Navigate to="/login" replace /> : <Project />
+                }
               ></Route>
               <Route
                 path="/login"
-                element={user ? <Navigate to="/" replace /> : <Login />}
+                element={
+                  // Redirect to the home page if the user is already authenticated, otherwise render Login
+                  user ? <Navigate to="/" replace /> : <Login />
+                }
               ></Route>
               <Route
                 path="/signup"
-                element={user ? <Navigate to="/" replace /> : <Signup />}
+                element={
+                  // Redirect to the home page if the user is already authenticated, otherwise render Signup
+                  user ? <Navigate to="/" replace /> : <Signup />
+                }
               ></Route>
             </Routes>
           </div>
+
+          {/* Render OnlineUsers component if the user is authenticated */}
           {user && <OnlineUsers />}
         </>
       )}
