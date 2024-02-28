f2a2ddf1c699cd2907c7057eb57c08854b2e5001
Mark Saville <mark.saville@outlook.com>
2024-02-28 16:02:22 +0100

    Update comments

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -6,20 +6,31 @@ import "./Project.css";
 import ProjectSummary from "./ProjectSummary";
 import ProjectComments from "./ProjectComments";
 
+// Component for displaying detailed information and comments for a project
 export default function Project() {
+  // Get the 'id' parameter from the route
   const { id } = useParams();
+
+  // Fetch project document using the 'useDocument' hook
   const { error, document } = useDocument("projects", id);
 
+  // If there's an error fetching the document, display the error message
   if (error) {
     return <div>{error.message}</div>;
   }
+
+  // If the document is still loading, display a loading message
   if (!document) {
     return <div>Loading...</div>;
   }
 
+  // Render the detailed information and comments for the project
   return (
     <div className="project-details">
+      {/* Render the summary of the project */}
       <ProjectSummary project={document} />
+
+      {/* Render the comments section for the project */}
       <ProjectComments project={document} />
     </div>
   );
